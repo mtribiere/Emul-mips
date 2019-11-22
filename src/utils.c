a8a30e81958e292d192fabba2f49158ded2fbeac
dautreym <51315556+dautreym@users.noreply.github.com>
2019-11-22 14:31:45 +0100

    Ajout Makefile basique (a refaire) + Ajout de fichiers dediés

--- src/utils.c
+++ src/utils.c
@@ -7,7 +7,7 @@
 
 //Fonction qui calcule la taille du code opération
 int getOperationSize(char *s){
-	
+
 	int size = 0;
 	while(s[size] != ' ')
 		size++;
@@ -15,32 +15,8 @@ int getOperationSize(char *s){
 	return size;
 }
 
-void printer(char instruction[])
-{
-	int size_of_instruction=strlen(instruction);
-
-	/* L'instruction, sans ses opérandes, est un mot de 2 à 4 lettres */ /* Peut être y en a à une lettre mais on verra */
-	/* Nous allons donc déterminer la longueur de l'instruction donnée pour faire une disjonction de cas */
-
-	if(instruction[2]==' ')
-	{
-		printf("\nL'instruction est composée de deux lettres.\n");
-		/* Faire ici la disjonction de cas pour afficher les instructions composées de deux lettres */
-	}
-	else if(instruction[3]==' ')
-	{
-		printf("\nL'instruction est composée de trois lettres.\n");
-		/* Faire ici la disjonction de cas pour afficher les instructions composées de trois lettres */
-	}
-	else if(instruction[4]==' ')
-	{
-		printf("\nL'instruction est composée de quatre lettres.\n");
-		/* Faire ici la disjonction de cas pour afficher les instructions composées de quatre lettres */
-
-
-		/* Premier cas : l'instruction ADDI : C EST DU PUTAIN DE BCD => lis les symboles 4 par 4*/
-		/* ATTENTION ORDRE RS ET RT 	tout écrire en binaire PUIS le réécrire en BCD => deux conversions à faire */
-		if(instruction[0]=='A' && instruction[1]=='D' && instruction[2]=='D' && instruction[3]=='I') printf("%x",0x200);
-	}
-	else printf("\nL'instruction donnée n'est pas conforme au langage MIPS.\n");
-}
+/* A FAIRE EN GLOBAL : une fonction convertHexaToBinary 
+        Prend en paramètre un nombre hexadécimal sous la forme d'un tableau de max 
+        Renvoie un tableau de cinq caractères ex. [11111] pour 31
+*/
+char *convertHexaToBinary()
