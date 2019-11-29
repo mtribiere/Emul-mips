d3939e1d8f6dbb0b9681dc8642a9fa7da4c21245
dautreym <51315556+dautreym@users.noreply.github.com>
2019-11-29 16:06:03 +0100

    Updated with fileInstructions && utils merged

--- src/main.c
+++ src/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "fileInstructions.h"
 #include "utils.h"
+
 
 int main(void)
 {
+	int index;
+
 	char *instruction;
 	instruction=malloc(sizeof(instruction));
+
+	char *opcode;
+	opcode=malloc(sizeof(opcode));
 
 	printf("\n========== Emul-mips qui claque ===========\n");
 
@@ -22,6 +29,11 @@ int main(void)
 	printf("Instruction numéro 3 du fichier in.txt : ");
 	readInstructionInFile("../testFiles/in.txt", 3, instruction);
 	printf("%s\n\n",instruction);
+
+	getOperationCode(instruction,opcode);
+	printf("Code opération : \n");
+	for(index=0;index<6;index++) printf("%d",opcode[index]);
+	printf("\n\n Fin affichage \n\n");
 
 	return 0;
 	//printf("Conversion hexa to binary : %s \n\n",convertHexaToBinary("2"));
